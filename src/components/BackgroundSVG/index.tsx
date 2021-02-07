8a1d9fe1bee0c34cfc9be3e054b4f41b7ed2d160
igorryan <igorryan10@gmail.com>
2021-02-07 04:44:50 -0300

    init

---
+++ src/components/BackgroundSVG/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './styles'
+
+const BackgroundSVG: React.FC = () => {
+  return (
+    <S.Wrapper />
+  )
+}
+
+export default BackgroundSVG;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import BackgroundSVG from '../../components/BackgroundSVG'
+
+const Dashboard: React.FC = () => {
+  return (
+    <BackgroundSVG />
+  )
+}
+
+export default Dashboard;
