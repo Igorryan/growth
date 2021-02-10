86f18901e58355ead1ab49cd3f9c1649fc52221c
igorryan <igorryan10@gmail.com>
2021-02-10 12:07:51 -0300

    Ajusting

--- src/components/BackgroundImage/index.tsx
+++ src/components/BackgroundImage/index.tsx
@@ -1,12 +1,12 @@
-import React, { HTMLAttributes, useState } from 'react';
+import React, { HTMLAttributes } from 'react';
 import * as S from './styles';
 import Background from '../../assets/img/background-1.png'
 
 const BackgroundImage: React.FC<HTMLAttributes<HTMLDivElement>> = ({children, ...props}) => {
-  const [background, setBackground] = useState(Background)
+  // const [background, setBackground] = useState(Background)
 
   return (
-    <S.Wrapper style={{backgroundImage: `url(${background})`}}>
+    <S.Wrapper style={{backgroundImage: `url(${Background})`}}>
       {children}
     </S.Wrapper>
   )
