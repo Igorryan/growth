7dc3b4b815cf2b573434aa692b3479975527a0c0
igorryan <igorryan10@gmail.com>
2021-02-25 12:50:00 -0300

    Background transition

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -6,24 +6,18 @@ import {useNavigation} from '../../hooks/navigation'
 const Background: React.FC = () => {
   const {background} = useNavigation()
   const [thisBackground, setThisBackground] = useState('')
-  const [height, setHeight] = useState('0')
 
   useEffect(() => {
-    setHeight('0')
-
     if(background === ''){
       console.log('background = ""')
       return
     }
 
-    setTimeout(() => {
       setThisBackground(background)
-      setHeight('100vh')
-    }, 300)
   }, [background, thisBackground])
 
   return (
-    <S.Wrapper style={{backgroundImage: `url(${thisBackground})`, height: height}} />
+    <S.Wrapper style={{backgroundImage: `url(${thisBackground})`}} />
   )
 }
 
--- src/components/Background/styles.ts
+++ src/components/Background/styles.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main`
-  /* background-position-y: -500px; */
-  /* background-position-x: -720px; */
   background-color: transparent;
   background-repeat: no-repeat;
   background-size: cover;
